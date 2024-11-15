5522ef712ebe7f1a97a4684eacd3188b0a22a5a4
Pau Vilella <pau.vilella@apple.com>
2024-11-15 10:37:59 +0100

    add treesitter

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -4,10 +4,7 @@ local map = vim.api.nvim_set_keymap
 
 vim.g.mapleader = " "
 
+o.mouse = ""
+o.clipboard = ""
 o.number = true
 o.relativenumber = true
-o.expandtab = false
-o.tabstop = 4
-o.shiftwidth = 4
-o.clipboard = ""
-o.mouse = ""
