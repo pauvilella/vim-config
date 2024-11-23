3e11cc3d67eb0bc42f769360bb658c40ad24fa89
Pau Vilella <pau.vilella@apple.com>
2024-11-23 15:25:29 +0100

    remove fugitive

--- lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	cmd = {"G", "Git"},
-	keys = {
-		{"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
-		{"<leader>gl", ":Git pull<cr>", desc = "Git pull"},
-	},
-}
